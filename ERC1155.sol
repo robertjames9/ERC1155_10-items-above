19b56530bc9175899f74083283e0779e8cc97f9d
temotee93 <95536550+temotee93@users.noreply.github.com>
2022-03-08 21:55:08 +0800

    Create ERC1155.sol

---
+++ ERC1155.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract SaveUkraineSociety is Ownable, ERC1155 {
+    string private baseURI;
+    string public name;
+
+    constructor() ERC1155(
+        "https://gateway.pinata.cloud/ipfs/QmYNJnVhj3qLoyrLtxXkehhFfp65WU73Hz2hWsBAjwsEm1/{id}.json"
+        ) 
+    {
+        setName("Save Ukraine Children");
+    }
+    
+    function setURI(string memory _newuri) public onlyOwner {
+        _setURI(_newuri);
+    }
+
+    function setName(string memory _name) public onlyOwner {
+        name = _name;
+    }
+
+    function mintBatch(uint256[] memory ids, uint256[] memory amounts)
+        public
+        onlyOwner
+    {
+        _mintBatch(msg.sender, ids, amounts, '');
+    }
+
+    function mint(uint256 id, uint256 amount) public onlyOwner {
+        _mint(msg.sender, id, amount, '');
+    }
+}
